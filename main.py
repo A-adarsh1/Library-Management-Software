c9b3367056ce49dc4d3203b5d7d437e5a020fc9d
Aadarsh <aadarshchaudhary04@gmail.com>
2024-07-22 21:30:11 +0545

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+class Library:
+    def __init__(self, no_of_books, books: list):
+        if books is None:
+            books = []
+        self.__no_of_books = no_of_books
+        self.__books = books
+
+    class __WrongArgumentValue(Exception):
+        def __init__(self, message):
+            super().__init__(message)
+            self.message = message
+
+    def books(self):
+        for book in self.__books:
+            print(book)
+
+    def add_book(self, book_name):
+        self.__books.append(book_name)
+        self.__no_of_books += 1
+
+    def total_books(self):
+        if self.__no_of_books == len(self.__books):
+            return self.__no_of_books
+        else:
+            raise self.__WrongArgumentValue("Number of books and Name of books are not Same.")
+
+
+janakpur_library = Library(5, ["Seto Bagh", "Summer Love", "Laxmi prasad Devkota", "Palpasa Cafe", "Seto Dharti"])
+total_books = janakpur_library.total_books()
+print(total_books)
+janakpur_library.books()
+janakpur_library.add_book("Buddha Darshan")
+print(janakpur_library.total_books())
+janakpur_library.books()
